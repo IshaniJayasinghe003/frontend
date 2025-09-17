d715f7dd65a0be0d760be32df20a7f4ffdd5f800
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-17 12:38:59 +0530

    🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
-import AdminPage from './pages/adminPage';
-import TestPage from './pages/testPage';
-import ClientwebPage from './pages/admin/client/clientPage';
-import ForgetPasswordPage from './pages/admin/client/forgetPassword';
+import AdminPage from "./pages/adminPage";
+import TestPage from "./pages/testPage";
 import { Toaster } from "react-hot-toast";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import ClientWebPage from "./pages/client/clientPage";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ForgetPasswordPage from "./pages/client/forgetPassword";
 
-const clientId = "809363394795-kfis3dejn0cec47dt4tf0pg8i5qkchd8.apps.googleusercontent.com";
+const clientId =
+  "1086978250852-q498jik4ukaq8q6m66ar26dgt6chjv24.apps.googleusercontent.com";
 
 function App() {
   return (
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={clientId}>
-        <div className="w-full h-screen flex justify-center bg-primary items-center text-secondary">
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <div className="w-full h-screen flex justify-center items-center bg-primary text-secondary">
           <Toaster position="top-right" />
-          <Routes path="/">
-            {/* Public Pages */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/forget" element={<ForgetPasswordPage />} />
-            <Route path="/test" element={<TestPage />} />
-
-            {/* Admin */}
-            <Route path="/admin/*" element={<AdminPage />} />
-
-            {/* Client Web Pages */}
-            <Route path="/*" element={<ClientwebPage />} />
-
-            {/* Optional HomePage */}
+          <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/test" element={<TestPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/admin/*" element={<AdminPage />} />
+            <Route path="/forget" element={<ForgetPasswordPage />} />
+            <Route path="/*" element={<ClientWebPage />} />
           </Routes>
         </div>
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
 }
 

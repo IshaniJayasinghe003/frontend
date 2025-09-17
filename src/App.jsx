2fb17c0c2525bacdda3f6b49c697ec1dd2885ba2
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-17 13:46:18 +0530

    🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
  import "./App.css"; 
-import HomePage from "./pages/homePage";
+ import HomePage from "./pages/homePage";
  import LoginPage from "./pages/loginPage";
  import RegisterPage from "./pages/registerPage"; 
-import AdminPage from "./pages/adminPage";
+ import AdminPage from "./pages/adminPage";
  import TestPage from "./pages/testPage";
  import { Toaster } from "react-hot-toast"; 
-import ClientWebPage from "./pages/client/ClientPage";
-
+ import ClientWebPage from "./pages/client/clientpage";
  import { GoogleOAuthProvider } from "@react-oauth/google";
  import ForgetPasswordPage from "./pages/client/forgetPassword";
 

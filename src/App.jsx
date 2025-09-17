1086e977eb2e4f2602a4f69942e5bb11c59e3f17
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-17 13:25:05 +0530

    🚀

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import RegisterPage from "./pages/registerPage";
 import AdminPage from "./pages/adminPage";
 import TestPage from "./pages/testPage";
 import { Toaster } from "react-hot-toast";
-import ClientWebPage from "./pages/client/clientWebPage";
+import ClientWebPage from "./pages/client/clientPage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import ForgetPasswordPage from "./pages/client/forgetPassword";
 
@@ -20,14 +20,14 @@ function App() {
         <div className="w-full h-screen flex justify-center items-center bg-primary text-secondary">
           <Toaster position="top-right" />
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} /> 
             <Route path="/login" element={<LoginPage />} />
             <Route path="/test" element={<TestPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/admin/*" element={<AdminPage />} />
             <Route path="/forget" element={<ForgetPasswordPage />} />
-            <Route path="/*" element={<ClientWebPage />} />
-          </Routes>
+            <Route path="/*" element={<ClientWebPage />} /> 
+            </Routes>
         </div>
       </BrowserRouter>
     </GoogleOAuthProvider>

76eacc7882e2efd86bad3ea2d0fe0daac3b24e5b
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-18 19:45:49 +0530

    chage login page

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center px-12">
+    <div className="w-full h-screen flex justify-start items-center px-12">
       <span className="text-3xl font-bold text-start text-accent">Evara Cosmetics that Everyone loves</span>
     
     </div>

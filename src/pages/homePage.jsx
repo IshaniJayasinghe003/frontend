19d0178a22b80f082904a5bb27e09fde55aa79b6
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-18 20:16:19 +0530

    changed left side things

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="w-full h-screen flex justify-start items-center px-12">
@@ -10,13 +9,10 @@ export default function HomePage() {
           Discover premium beauty products crafted with care and loved by
           everyone. Enhance your style with Evara Cosmetics.
         </p>
-        <Link
-          to="/products"
-          className="inline-block px-10 py-4 bg-pink-500 text-white text-lg font-semibold rounded-full shadow-lg hover:bg-pink-600 hover:scale-105 transition-transform duration-300"
-        >
+        <button className="px-10 py-4 bg-pink-500 text-white text-lg font-semibold rounded-full shadow-lg hover:bg-pink-600 hover:scale-105 transition-transform duration-300">
           Explore Products
-        </Link>
-      </div>
+        </button>
+        </div>
     </div>
   );
 }

0197bdd8f43a57445861bfeae0a6f48ccf1a8654
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-19 08:17:24 +0530

    remove name

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
     <img src="face.jpg" className="w-full h-full object-cover"/>
-      <span className="text-3xl font-bold">Home Page</span>
+      
     </div>
   )
 }

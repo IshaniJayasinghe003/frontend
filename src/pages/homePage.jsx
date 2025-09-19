e89b4c580ff2b1658867cd5f9987c14951a7ebf2
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-19 08:20:35 +0530

    add name

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,10 @@ export default function HomePage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
     <img src="face.jpg" className="w-full h-full object-cover"/>
-      
+    <div className="absolute top-6 left-8 text-white text-2xl font-bold z-20">
+        🌸 Evara Cosmetics
+      </div>
+
     </div>
   )
 }

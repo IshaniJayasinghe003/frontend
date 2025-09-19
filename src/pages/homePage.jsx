21e7f76fa2176aab915a4288cb441361f88dbf91
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-19 08:29:49 +0530

    center the text

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,18 @@
 export default function HomePage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-    <img src="face.jpg" className="w-full h-full object-cover"/>
-    <div className="absolute top-6 left-8  text-centerfont-sans text-black text-4xl font-bold z-20">
-      Evara Cosmetics 
-      </div>
+    <div className="relative w-full h-screen flex">
+      
+      {/* Background Image */}
+      <img 
+        src="face.jpg" 
+        alt="Hero" 
+        className="w-full h-full object-cover" 
+      />
 
+      {/* Website Name */}
+      <div className="absolute left-10 top-1/2 -translate-y-1/2 text-black text-5xl font-bold font-sans z-20">
+        Evara Cosmetics
+      </div>
     </div>
   )
 }

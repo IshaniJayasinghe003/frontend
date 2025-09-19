8bea608515f50bae1788e82aa0613d22bef374f4
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-19 08:25:05 +0530

    change text

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
     <img src="face.jpg" className="w-full h-full object-cover"/>
-    <div className="absolute top-6 left-8 text-white text-2xl font-bold z-20">
+    <div className="absolute top-6 left-8  text-centerfont-sans text-black text-4xl font-bold z-20">
       Evara Cosmetics 
       </div>
 

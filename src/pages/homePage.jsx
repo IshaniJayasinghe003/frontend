16bcaf8e81397c50f93aa1282e47bd07bd2b6223
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-19 08:12:30 +0530

    add new image

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-    <img src="hero_bg.jpg" className="w-full h-full object-cover"/>
-      <span>Home Page</span>
+    <img src="face.jpg" className="w-full h-full object-cover"/>
+      <span className="text-3xl font-bold">Home Page</span>
     </div>
   )
 }

929efd2b917557b62ff015ec28b6d12ad879e484
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-22 17:30:39 +0530

    add cover page

--- src/pages/aboutUsPage.jsx
+++ src/pages/aboutUsPage.jsx
@@ -1,15 +1,15 @@
 export default function AboutUsPage() {
   return (
-<div className="w-full h-screen flex justify-center items-center">
-<img 
-          src="usp.webp" 
-          alt="Evara Cosmetics Cover" 
-          className="rounded-2xl shadow-lg w-full max-w-md object-cover mb-6"
-        />
-        <h1>About Us</h1>
-</div>
+    <div className="w-full h-screen relative">
+      {/* Cover Image */}
+      <img 
+        src="usp.webp" 
+        alt="Evara Cosmetics Cover" 
+        className="w-full h-full object-cover"
+      />
 
-
-  );
+      {/* Overlay Text */}
+      
+    </div>
+  )
 }
-   

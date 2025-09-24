5cdd163b7e212343d6d60534a6a6870906c956f5
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-24 21:32:20 +0530

    add center to the text

--- src/pages/reviewsPage.jsx
+++ src/pages/reviewsPage.jsx
@@ -1,7 +1,9 @@
 export default function ReviewsPage() {
-    return(
-        <div className="w-full h-screen">
-            <span>Reviews Page</span>
-        </div>
-    )
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-gray-50">
+      <span className="text-2xl font-semibold text-gray-800">
+        Reviews Page
+      </span>
+    </div>
+  );
 }

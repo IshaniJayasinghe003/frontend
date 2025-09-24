1d22e302f4b4d35c87bff53e7fe147edd4a36b39
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-24 09:46:33 +0530

    change the image width

--- src/pages/aboutUsPage.jsx
+++ src/pages/aboutUsPage.jsx
@@ -6,7 +6,7 @@ export default function AboutUsPage() {
   return (
     <div className="w-full h-screen flex">
       {/* Left Side - Image */}
-      <div className="w-1/2 h-full">
+      <div className="w-full h-full">
         <img
           src="face.jpg"
           alt="Evara Cosmetics Cover"

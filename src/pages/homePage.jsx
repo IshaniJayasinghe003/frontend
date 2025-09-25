b565c0337996528a6d10bf39b7a932ab36cdc9b9
ishani jayasinghe <ishanijayasinghe78@gmail.com>
2025-09-25 19:23:21 +0530

    remove the black color line

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -5,11 +5,11 @@ export default function HomePage() {
     <div className="w-full min-h-screen relative">
 
       {/* Top Navigation Links */}
-      <div className="absolute top-0 left-0 w-full flex justify-center gap-4 sm:gap-8 p-4 z-20 bg-black/30">
+      <div className="absolute top-0 left-0 w-full flex justify-center gap-4 sm:gap-8 p-4 z-20 ">
         <Link to="/reviews" className="text-white text-xl font-semibold hover:text-white transition">
           Reviews
         </Link>
-        <Link to="/about-us" className="text-white text-xlfont-semibold hover:text-whitetransition">
+        <Link to="/about-us" className="text-white text-xl font-semibold hover:text-whitetransition">
           About Us
         </Link>
         <Link to="/contact-us" className="text-white text-xl font-semibold hover:text-white transition">
